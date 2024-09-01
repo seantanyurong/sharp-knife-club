015521380a6c65d743d7465c6b221091225f43c3
Sean Tan <sean.tan@sean.tan-GHKX6X1N9C>
2024-09-01 14:35:20 +0800

    feature: add form

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,9 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <main>
-      <div className='bg-primary text-primary-foreground py-4 md:py-8 px-4'>
+      <div className='bg-primary text-primary-foreground py-4 md:py-6 px-6'>
         <div className='max-w-6xl flex justify-between items-center mx-auto'>
-          <h1 className='text-lg md:text-3xl font-semibold'>Sharp Knife Club</h1>
+          <Image src={'/logo.png'} alt='Sharp Knife Club' width={80} height={80} priority />
           <div className='flex gap-6'>
             <a href='https://wa.me/6596770773' className='items-center gap-1 hidden md:flex'>
               <svg
@@ -26,29 +26,33 @@ export default function Home() {
               </svg>
               <p className='text-lg font-semibold'>(65) 9677 0773</p>
             </a>
-            <Button variant={'secondary'} size={'lg'} className='text-lg font-semibold hidden md:block'>
-              Join Waitlist
-            </Button>
-            <Button variant={'secondary'} size={'sm'} className='text-sm font-semibold block md:hidden'>
-              Join Waitlist
-            </Button>
+            <a href='https://szuv34ek50v.typeform.com/to/p8aeHQyl'>
+              <Button variant={'secondary'} size={'lg'} className='text-lg font-semibold hidden md:block'>
+                Join Waitlist
+              </Button>
+            </a>
+            <a href='https://szuv34ek50v.typeform.com/to/p8aeHQyl'>
+              <Button variant={'secondary'} size={'sm'} className='text-sm font-semibold block md:hidden'>
+                Join Waitlist
+              </Button>
+            </a>
           </div>
         </div>
       </div>
       <div className='h-[65vh] w-screen relative'>
-        <Image src={'/images/hero.jpg'} alt='Sharp Knife Club' fill objectFit='cover' priority />
+        <Image src={'/images/hero.jpg'} alt='Sharp Knife Club' fill className='object-cover' priority />
       </div>
-      <div className='max-w-2xl mx-auto py-16 px-4'>
+      <div className='max-w-2xl mx-auto pt-16 pb-8 px-6'>
         <h1 className='text-5xl font-bold mb-8'>Hello!</h1>
-        <p className='text-xl mb-8'>
+        <p className='md:text-xl text-lg mb-8'>
           We help pick up and sharpen knives for over 500 home cooks and professional chefs. Let us help you next!
         </p>
         <div className='mb-16 bg-accent text-accent-foreground p-4 rounded-lg'>
           <h3 className='text-2xl font-bold'>How does it work?</h3>
           <br></br>
-          <p className='text-xl'>It&apos;s a simple 3 step process.</p>
+          <p className='md:text-xl text-lg'>It&apos;s a simple 3 step process.</p>
           <br></br>
-          <ul className='text-xl list-disc pl-5'>
+          <ul className='md:text-xl text-lg list-disc pl-5'>
             <li className='mb-4'>
               <b>Drop Off:</b> Leave your knives at one of our many locations across Singapore.
             </li>
@@ -64,32 +68,32 @@ export default function Home() {
 
         <h1 className='text-3xl font-bold mb-8'>How much do you charge?</h1>
         <div className='mb-8'>
-          <p className='text-xl'>
+          <p className='md:text-xl text-lg'>
             Prices are inclusive of pick up and drop off fees. A minimum of 3 knives are required.
           </p>
         </div>
         <div className='mb-8 bg-secondary text-secondary-foreground p-4 rounded-lg'>
-          <h3 className='text-xl font-bold mb-4'>Basic - $10 / knife</h3>
-          <p className='text-xl'>
+          <h3 className='md:text-xl text-lg font-bold mb-4'>Basic - $10 / knife</h3>
+          <p className='md:text-xl text-lg'>
             This is for your standard kitchen knife. Get it back as sharp as when you first bought it, guaranteed. It
             will glide through paper and slice meat effortlessly. <i>No serrated knives or scissors.</i>
           </p>
         </div>
         <div className='mb-16 bg-secondary text-secondary-foreground p-4 rounded-lg'>
-          <h3 className='text-xl font-bold'>Premium - $25 / knife</h3>
+          <h3 className='md:text-xl text-lg font-bold'>Premium - $25 / knife</h3>
           <br></br>
-          <p className='text-xl'>
+          <p className='md:text-xl text-lg'>
             This is for your high-end kitchen knives, japanese knives, etc. Our experts will sharpen it with the best
             treatment according to its individual material, design and anatomy.
           </p>
           <br></br>
-          <p className='text-xl'>
+          <p className='md:text-xl text-lg'>
             It will be hand sharpened with whetstones, up to 50,000 grit, and returned with a mirror finish.
           </p>
         </div>
         <h1 className='text-3xl font-bold mb-8'>Why should I sharpen my knives?</h1>
         <div className='mb-16'>
-          <ul className='text-xl list-disc pl-5'>
+          <ul className='md:text-xl text-lg list-disc pl-5'>
             <li className='mb-4'>
               <b>Enjoyable:</b> Cooking with dull knives is frustrating and time-consuming. Make cooking pleasurable and
               faster with sharp knives.
@@ -106,7 +110,7 @@ export default function Home() {
         </div>
         <h1 className='text-3xl font-bold mb-8'>Why choose Sharp Knife Club?</h1>
         <div className='mb-16'>
-          <ul className='text-xl list-disc pl-5 mb-8'>
+          <ul className='md:text-xl text-lg list-disc pl-5 mb-8'>
             <li className='mb-4'>
               <b>Convenience:</b> We make it easy for you. With multiple drop-off points across Singapore, you can
               conveniently leave your knives with us, and we’ll take care of the rest. No need to worry about making
@@ -121,7 +125,7 @@ export default function Home() {
               techniques and the finest grit to ensure a razor-sharp edge with a flawless finish.
             </li>{' '}
           </ul>
-          <p className='text-xl'>
+          <p className='md:text-xl text-lg'>
             Questions?{' '}
             <a
               href='https://wa.me/6596770773'
@@ -131,6 +135,25 @@ export default function Home() {
           </p>
         </div>
       </div>
+      <footer>
+        <div className='max-w-2xl mx-auto pb-16 px-6'>
+          <p className='text-center text-sm text-gray-500 mb-1 font-bold'>Sharp Knife Club SG.</p>
+          <p className='text-center text-sm text-gray-500 mb-1'>
+            Pick-up knife sharpening service for more than 500 homecooks and professional chefs.
+          </p>
+          <p className='text-center text-sm text-gray-500'>&copy; 2024. All rights reserved.</p>
+        </div>
+      </footer>
+      <a href='https://szuv34ek50v.typeform.com/to/p8aeHQyl'>
+        <Button size={'lg'} className='text-lg font-semibold hidden md:block fixed bottom-6 right-6'>
+          Join Waitlist
+        </Button>
+      </a>
+      <a href='https://szuv34ek50v.typeform.com/to/p8aeHQyl'>
+        <Button size={'sm'} className='text-sm font-semibold block md:hidden fixed bottom-4 right-4'>
+          Join Waitlist
+        </Button>
+      </a>
     </main>
   );
 }
