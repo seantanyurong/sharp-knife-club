13773417b50c428bd230f90f66ce5115bdd7edea
Sean Tan <seantanyurong@gmail.com>
2025-03-11 16:19:55 +0800

    feature: add autoplay to reviews

--- components/googleReview.tsx
+++ components/googleReview.tsx
@@ -2,7 +2,7 @@ import { Star } from 'lucide-react';
 
 export default function GoogleReview({ name, rating, comment }: { name: string; rating: number; comment: string }) {
   return (
-    <div className='bg-white p-4 rounded-xl shadow-md max-w-sm border h-[260px] flex flex-col justify-between'>
+    <div className='bg-white p-4 rounded-xl shadow-md max-w-sm border h-[275px] flex flex-col justify-between'>
       <div>
         <div className='flex items-center space-x-3'>
           <div className='bg-gray-300 text-white w-10 h-10 flex items-center justify-center rounded-full text-lg font-bold'>
