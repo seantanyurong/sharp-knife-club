f6e8e3a680c9970e7a0b870e2583bff3cbd3208a
joesurf <desictiverk@gmail.com>
2025-03-18 01:39:22 +0800

    Add A/B test for pricing section positioning

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,7 @@ import FeaturedSection from './blog/featured_section';
 import WhatsAppLink from '@/components/ui/whatsapp';
 import PostHogEventCapture from '@/components/ui/posthogeventcapture';
 import GoogleReviewCarousel from '@/components/googleReviewCarousel';
+import PricingSection from '@/components/pricingsection';
 
 export default function Home() {
   return (
@@ -132,6 +133,8 @@ export default function Home() {
             </div>
           </div>
         </div>
+
+        <PricingSection control={false} />
         {/* REVIEWS */}
         <GoogleReviewCarousel />
         {/* INSTRUCTIONS */}
@@ -167,35 +170,7 @@ export default function Home() {
           </div>
         </div>
         {/* PRICING */}
-        <div className='max-w-6xl mx-auto grid grid-cols-1 lg:grid-cols-3 lg:gap-8 px-6 mt-16'>
-          <div className='col-span-1'>
-            <Image src={'/images/price.png'} alt='Price' width={500} height={500} className='rounded-2xl' />
-          </div>
-          <div className='col-span-2 mt-8 lg:mt-0'>
-            <h1 className='text-primary text-3xl md:text-4xl font-black'>Pricing</h1>
-            <div className='mt-4 flex gap-4 flex-col'>
-              <div className='bg-muted rounded-2xl p-4'>
-                <h1 className='text-lg md:text-2xl font-black'>1 Blade — $30 / knife</h1>
-              </div>
-              <div className='bg-muted rounded-2xl p-4'>
-                <h1 className='text-lg md:text-2xl font-black'>2 Blades — $20 / knife</h1>
-              </div>
-              <a href='https://wa.me/message/LQDK2KE5I3PNF1'>
-                <div className='bg-primary rounded-2xl p-4 relative'>
-                  <h1 className='text-lg md:text-2xl text-white font-black'>3+ Blades — $15 / knife</h1>
-                  <Image
-                    src={'/images/best-deal.png'}
-                    alt='Price'
-                    width={80}
-                    height={80}
-                    className='absolute -top-8 -right-6 z-99'
-                  />
-                </div>
-              </a>
-            </div>
-            <p className='mt-4 italic'>Additional costs apply for major repairs, de-rusting and serrated knives.</p>
-          </div>
-        </div>
+        <PricingSection control={true} />
         {/* CAROUSEL */}
         {/* <div className='mt-16 px-6'>
           <Carousel
---
+++ components/pricingsection.tsx
@@ -0,0 +1,61 @@
+'use client'
+
+import Image from 'next/image'
+import posthog from 'posthog-js';
+import { useState, useEffect } from 'react';
+
+function PricingSection({ control=true }: { control: boolean; }) {
+  const [showFeature, setShowFeature] = useState<boolean>(false);
+
+  useEffect(() => {
+    const flag = posthog.getFeatureFlag('pricing-positioning-conversion') || "control";
+    console.log(flag, control)
+    if (flag === 'position-top' && !control) {
+      console.log("Top");
+      setShowFeature(true);
+    }
+
+    if (flag !== 'position-top' && control) {
+      console.log("Bottom");
+      setShowFeature(true);
+    }
+  }, []);
+
+  if (!showFeature) {
+    return null;
+  }
+
+  return (
+    <div className='max-w-6xl mx-auto grid grid-cols-1 lg:grid-cols-3 lg:gap-8 px-6 mt-16'>
+      <div className='col-span-1'>
+        <Image src={'/images/price.png'} alt='Price' width={500} height={500} className='rounded-2xl' />
+      </div>
+      <div className='col-span-2 mt-8 lg:mt-0'>
+        <h1 className='text-primary text-3xl md:text-4xl font-black'>Pricing</h1>
+        <div className='mt-4 flex gap-4 flex-col'>
+          <div className='bg-muted rounded-2xl p-4'>
+            <h1 className='text-lg md:text-2xl font-black'>1 Blade — $30 / knife</h1>
+          </div>
+          <div className='bg-muted rounded-2xl p-4'>
+            <h1 className='text-lg md:text-2xl font-black'>2 Blades — $20 / knife</h1>
+          </div>
+          <a href='https://wa.me/message/LQDK2KE5I3PNF1'>
+            <div className='bg-primary rounded-2xl p-4 relative'>
+              <h1 className='text-lg md:text-2xl text-white font-black'>3+ Blades — $15 / knife</h1>
+              <Image
+                src={'/images/best-deal.png'}
+                alt='Price'
+                width={80}
+                height={80}
+                className='absolute -top-8 -right-6 z-99'
+              />
+            </div>
+          </a>
+        </div>
+        <p className='mt-4 italic'>Additional costs apply for major repairs, de-rusting and serrated knives.</p>
+      </div>
+    </div>
+  )
+}
+
+export default PricingSection
