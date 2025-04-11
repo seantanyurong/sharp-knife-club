3ea2d7dba686276601c78ce789d73dbe736fbd0e
joesurf <desictiverk@gmail.com>
2025-04-11 16:08:21 +0800

    Update dates dynamic

--- constants/dates.tsx
+++ constants/dates.tsx
@@ -1,23 +1,25 @@
-// const getNextPickupDate = (): string => {
-//   const startDate = new Date(Date.UTC(2025, 1, 1));
-//   const currentDate = new Date();
-//   const currentDateUTC = new Date(
-//     Date.UTC(currentDate.getUTCFullYear(), currentDate.getUTCMonth(), currentDate.getUTCDate()),
-//   );
+const getNextPickupDate = (): string => {
+  const startDate = new Date(Date.UTC(2025, 1, 1));
+  const currentDate = new Date();
+  const currentDateUTC = new Date(
+    Date.UTC(currentDate.getUTCFullYear(), currentDate.getUTCMonth(), currentDate.getUTCDate()),
+  );
 
-//   const diffInWeeks = Math.floor((currentDateUTC.getTime() - startDate.getTime()) / (1000 * 60 * 60 * 24 * 7));
+  const diffInWeeks = Math.floor((currentDateUTC.getTime() - startDate.getTime()) / (1000 * 60 * 60 * 24 * 7));
 
-//   const nextPickupWeek = diffInWeeks % 2 === 0 ? diffInWeeks + 2 : diffInWeeks + 1;
+  const nextPickupWeek = diffInWeeks % 2 === 0 ? diffInWeeks + 2 : diffInWeeks + 1;
 
-//   const nextPickupDate = new Date(startDate);
-//   nextPickupDate.setDate(startDate.getDate() + nextPickupWeek * 7);
+  const nextPickupDate = new Date(startDate);
+  nextPickupDate.setDate(startDate.getDate() + nextPickupWeek * 7);
 
-//   const dayOfWeek = nextPickupDate.getUTCDay();
-//   const daysUntilSaturday = (6 - dayOfWeek + 7) % 7;
-//   nextPickupDate.setDate(nextPickupDate.getDate() + daysUntilSaturday);
+  const dayOfWeek = nextPickupDate.getUTCDay();
+  const daysUntilSaturday = (6 - dayOfWeek + 7) % 7;
+  nextPickupDate.setDate(nextPickupDate.getDate() + daysUntilSaturday);
 
-//   const options: Intl.DateTimeFormatOptions = { weekday: 'long', day: 'numeric', month: 'short', year: 'numeric' };
-//   return nextPickupDate.toLocaleDateString('en-US', options);
-// };
+  const options: Intl.DateTimeFormatOptions = { weekday: 'long', day: 'numeric', month: 'short', year: 'numeric' };
+  return nextPickupDate.toLocaleDateString('en-US', options);
+};
 
-export const NEXT_PICKUP_DATE = 'Apr 5, 2025';
+export const NEXT_PICKUP_DATE = getNextPickupDate();
+
+// export const NEXT_PICKUP_DATE = 'Apr 5, 2025';
