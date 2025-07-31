67cf46a676285fd2ebd044ea6f886b3aa06c8983
seantanyurong <seantanyurong@gmail.com>
2025-07-31 15:11:38 +0800

    feature: update reviews; update review button

--- components/googleReviewButton.tsx
+++ components/googleReviewButton.tsx
@@ -4,11 +4,8 @@ import { Star } from 'lucide-react';
 import React from 'react';
 import { Button } from '@/components/ui/button';
 import GoogleLogo from '@/components/ui/googlelogo';
-import CopyChangeWrapper from './abtest/CopyChangeWrapper';
 
 const GoogleReviewButton = () => {
-  const showGoogleReviewsVariant = CopyChangeWrapper({ feature: 'hero-section-google-reviews-button', children: null }) as boolean;
-
   return (
     <a href='https://g.co/kgs/aXcTBcs' target='_blank' rel='noreferrer' className='block w-full lg:w-auto'>
       <Button
@@ -16,20 +13,19 @@ const GoogleReviewButton = () => {
         size={'2xl'}
         className='text-xl font-black flex flex-col gap-0 w-full lg:w-auto'>
         <div className='flex items-center gap-1'>
-          {showGoogleReviewsVariant && <GoogleLogo />}
-          5.0
+          <GoogleLogo />
+          <span className='ml-1'>5.0
+          </span>
           <div className='flex'>
             {[...Array(5)].map((_, index) => (
               <Star key={index} className='w-12 h-12 text-secondary fill-secondary' />
             ))}
           </div>
-          {showGoogleReviewsVariant && (
-            <span className='text-xs text-gray-500 font-medium'>
-              (82)
-            </span>
-          )}
+          <span className='text-xs text-gray-500 font-medium'>
+            (92)
+          </span>
         </div>
-        <p className='text-sm text-gray-500 font-medium underline'>No.1 Reviewed in SG</p>
+        <p className='text-sm text-gray-500 font-medium underline'>No.1 Google Reviews in SG</p>
       </Button>
     </a>
   );
--- components/googleReviewCarousel.tsx
+++ components/googleReviewCarousel.tsx
@@ -7,27 +7,10 @@ import GoogleReview from '@/components/googleReview';
 
 const googleReviews = [
   {
-    name: 'Paul Martin',
+    name: 'Ken Chang',
     rating: 5,
     comment:
-      'Excellent service. Great communication throughout with recommendations based on the actual knife condition. Returned as promised within 24 hours, even on a weekend. The knives are in superb condition now. Much appreciated. Will definitely use them again. Highly recommended.',
-  },
-  {
-    name: 'Oz S Perry',
-    rating: 5,
-    comment:
-      'Great service fast and reliable. The workmanship was good and the knives were really sharp. Will use the service again.',
-  },
-  {
-    name: 'Bernard Raj',
-    rating: 5,
-    comment:
-      'I recently sent my knives for sharpening, and I couldn’t be happier with the results. The edges are razor-sharp, better than when they were brand new! The service was efficient, professional, and truly exceeded my expectations.',
-  },
-  {
-    name: 'Ann Williams',
-    rating: 5,
-    comment: 'Very prompt, friendly service! My set of knives came back very nicely sharpened the very next day.',
+      'Excellent service. Sean did a great job sharpening my Japanese knives and a chinese chopper. Tested them with the newspaper cutting test and passed beautifully. Prompt pick up and turnaround too! Would recommend their service for anyone looking to sharpen their knives.',
   },
   {
     name: 'Z K',
@@ -36,16 +19,39 @@ const googleReviews = [
       'The service is excellent. All you need to do is just to leave the knives at your doorstep in the morning and they will all be back in original sharpness in the morning next day. So convenient, pick up and delivery are FOC, and they only charge $15 per knife!',
   },
   {
+    name: 'JC - Local Guide',
+    rating: 5,
+    comment:
+      'Massive kudos to Sean and the Knife Sharpening SG crew! ⚡🔪 Fast, friendly, and freakishly convenient — pick-up and drop-off included in the price (yes, really!) 🚚💸 Sean’s super responsive and just an all-round awesome guy 🙌 My knives came back scary sharp — in the best way. They’re sharper than my jokes now! 😅🔥',
+  },
+  {
+    name: 'Bernard Raj',
+    rating: 5,
+    comment:
+      'I recently sent my knives for sharpening, and I couldn’t be happier with the results. The edges are razor-sharp, better than when they were brand new! The service was efficient, professional, and truly exceeded my expectations.',
+  },
+  {
+    name: 'Eugene Chang',
+    rating: 5,
+    comment: 'If you’re looking for a place to sharpen those knives of yours, look no further than this! Excellent and top notch service. He will pickup and deliver back to you with no worries. Love the quality of the finishing of my kitchen knives. Thank you very much Sean.',
+  },
+  {
     name: 'Hong Lynn Chen',
     rating: 5,
     comment:
       'Randomly googled as my knives were in dire need of sharpening. Knife Sharpening SG came up and - zero regrets! They pick up and return within 24 hours but I needed them to delay return by a day and they were happy to do that, my knives are super sharp now.',
   },
   {
-    name: 'Jonas Tan',
+    name: 'Paul Martin',
     rating: 5,
     comment:
-      'Sent in 5 knives for sharpening and couldn’t be happier with the results! All the knives were returned razor-sharp, making cooking a breeze again. The pick-up and delivery service straight to my doorstep also made the whole process super convenient. Highly recommend!',
+      'Excellent service. Great communication throughout with recommendations based on the actual knife condition. Returned as promised within 24 hours, even on a weekend. The knives are in superb condition now. Much appreciated. Will definitely use them again. Highly recommended.',
+  },
+  {
+    name: 'Doreen Leong',
+    rating: 5,
+    comment:
+      'Extremely satisfied with the exceptional service from Knife Sharpening Singapore! The seamless process included prompt 24-hour pickup and return, with knives carefully wrapped and protected. The pricing was reasonable, and the sharpening work was impeccable. Thank you for a job well done!',
   },
   {
     name: 'Jonathan Jerome',
@@ -54,64 +60,15 @@ const googleReviews = [
       'Great experience overall. Very satisfied with the end result and the expedient pick up and drop off was a pleasant surprise. Will definitely be using this service in the future.',
   },
   {
-    name: 'Benedict Lee',
+    name: 'Andrew Deane',
     rating: 5,
-    comment: 'The knives that you have sharpened are wonderfully sharpened. A job well done!',
+    comment: 'Exceptional Knife Sharpening Service – Like Factory New! I recently entrusted this company with 11 of my cherished Global knives, and I couldn’t be more impressed with the results. These knives are the heart of my kitchen and see heavy daily use, so I was a bit nervous handing them over. But wow—when they were returned, they looked and felt almost like they had just come out of the factory!',
   },
   {
     name: 'Alessandro Felicani',
     rating: 5,
     comment:
       'Great 24hr service with home pick up and delivery. Sharpened as expected with fast and smooth communication! I might try the service for my Japanese knives and fixing some chipped ones in the future!',
-  },
-  {
-    name: 'Alexander Sim',
-    rating: 5,
-    comment:
-      'Highly professional - punctual pick up from home (at no additional cost) and delivery (at no additional cost too). Knives are as sharp as samurai swords now!',
-  },
-  {
-    name: 'Joseph Ong',
-    rating: 5,
-    comment:
-      'I had to sharpen a knife urgently to cook for a family gathering - and they were able to do it quite quickly. Within a few days of handing over my knife, I got it back sharpened. Quite affordable too for a pick-up service. Will use again.',
-  },
-  {
-    name: 'Y A',
-    rating: 5,
-    comment:
-      'Incredibly efficient pick up and re-delivery of knives. Great pricing and most importantly, the knives were sharpened really well. Thank you!',
-  },
-  {
-    name: 'Raymond KONG',
-    rating: 5,
-    comment:
-      'SKC Service was especially suitable for those with a busy schedule. Knife was razor sharp after the process. Overall experience was good!',
-  },
-  {
-    name: 'Bckt',
-    rating: 5,
-    comment: 'Quick and easy service. Knives nicely sharpened. Great service.',
-  },
-  {
-    name: 'Pu Xiaorong',
-    rating: 5,
-    comment: 'Impressed with the service, direct pick up from home and return following day, good customer service.',
-  },
-  {
-    name: 'Ataru Onuma',
-    rating: 5,
-    comment: 'Quick high-quality service. And good price.',
-  },
-  {
-    name: 'Lalitha G',
-    rating: 5,
-    comment: 'Awesome service. Prompt and Professional.',
-  },
-  {
-    name: 'C Phang',
-    rating: 5,
-    comment: 'Good service with fast turnaround.',
   },
 ];
 
