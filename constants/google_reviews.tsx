7365dfdc560cf8d43b4564baad083dcd63342336
joesurf <desictiverk@gmail.com>
2025-08-04 17:23:12 +0800

    fix image src

--- constants/google_reviews.tsx
+++ constants/google_reviews.tsx
@@ -72,6 +72,7 @@ export const googleReviews = [
     handle: "90+ reviews",
     rating: 5,
     comment: '',
+    image: false,
   },
   {
     src: "Andrew-Deane",
@@ -79,6 +80,7 @@ export const googleReviews = [
     handle: "10+ reviews",
     rating: 5,
     comment: 'Exceptional Knife Sharpening Service – Like Factory New! I recently entrusted this company with 11 of my cherished Global knives, and I couldn’t be more impressed with the results. These knives are the heart of my kitchen and see heavy daily use, so I was a bit nervous handing them over. But wow—when they were returned, they looked and felt almost like they had just come out of the factory!',
+    image: false,
   },
   {
     src: "Alessandro-Felicani",
@@ -87,6 +89,7 @@ export const googleReviews = [
     rating: 5,
     comment:
       'Great 24hr service with home pick up and delivery. Sharpened as expected with fast and smooth communication! I might try the service for my Japanese knives and fixing some chipped ones in the future!',
+    image: false,
   },
   {
     src: "Ken-Chang",
@@ -95,6 +98,7 @@ export const googleReviews = [
     rating: 5,
     comment:
       'Excellent service. Sean did a great job sharpening my Japanese knives and a chinese chopper. Tested them with the newspaper cutting test and passed beautifully. Prompt pick up and turnaround too! Would recommend their service for anyone looking to sharpen their knives.',
+    image: false,
   },
   {
     src: "Z-K",
@@ -103,6 +107,7 @@ export const googleReviews = [
     rating: 5,
     comment:
       'The service is excellent. All you need to do is just to leave the knives at your doorstep in the morning and they will all be back in original sharpness in the morning next day. So convenient, pick up and delivery are FOC, and they only charge $15 per knife!',
+    image: false,
   },
   {
     src: "JC",
@@ -111,6 +116,7 @@ export const googleReviews = [
     rating: 5,
     comment:
       'Massive kudos to Sean and the Knife Sharpening SG crew! ⚡🔪 Fast, friendly, and freakishly convenient — pick-up and drop-off included in the price (yes, really!) 🚚💸 Sean’s super responsive and just an all-round awesome guy 🙌 My knives came back scary sharp — in the best way. They’re sharper than my jokes now! 😅🔥',
+    image: false,
   },
   {
     src: "Bernard-Raj",
@@ -119,6 +125,7 @@ export const googleReviews = [
     rating: 5,
     comment:
       'I recently sent my knives for sharpening, and I couldn’t be happier with the results. The edges are razor-sharp, better than when they were brand new! The service was efficient, professional, and truly exceeded my expectations.',
+    image: false,
   },
   {
     src: "Hong-Lynn-Chen",
@@ -127,5 +134,6 @@ export const googleReviews = [
     rating: 5,
     comment:
       'Randomly googled as my knives were in dire need of sharpening. Knife Sharpening SG came up and - zero regrets! They pick up and return within 24 hours but I needed them to delay return by a day and they were happy to do that, my knives are super sharp now.',
+    image: false,
   },
 ]
