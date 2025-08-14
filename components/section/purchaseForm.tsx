944c710d2c6227fe09fd6e2c383c2b8209fffa1b
seantanyurong <seantanyurong@gmail.com>
2025-08-14 16:55:02 +0800

    feature: placeholder form

--- components/section/purchaseForm.tsx
+++ components/section/purchaseForm.tsx
@@ -10,7 +10,10 @@ import {
   DrawerTitle,
 } from "@/components/ui/drawer"
 import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
 import { usePurchaseForm } from "@/context/purchaseForm"
+import NextPickUpDate from "@/components/NextPickupDate"
 
 export default function PurchaseForm() {
   const { isOpen, setIsOpen } = usePurchaseForm()
@@ -19,16 +22,31 @@ export default function PurchaseForm() {
     <Drawer open={isOpen} onOpenChange={setIsOpen}>
       <DrawerContent>
         <DrawerHeader>
-          <DrawerTitle>Are you absolutely sure?</DrawerTitle>
-          <DrawerDescription>This action cannot be undone.</DrawerDescription>
+          <DrawerTitle>Let's get your knives sharp again!</DrawerTitle>
+          <DrawerDescription>Free doorstep pickup and delivery.</DrawerDescription>
         </DrawerHeader>
+        <div className="px-4">
+          <form className="grid items-start gap-6">
+            <div className="grid gap-3">
+              <Label htmlFor="email">Email</Label>
+              <Input type="email" id="email" defaultValue="shadcn@example.com" />
+            </div>
+            <div className="grid gap-3">
+              <Label htmlFor="username">Username</Label>
+              <Input id="username" defaultValue="@shadcn" />
+            </div>
+            <div className='text-sm'>
+              Next Collection: <NextPickUpDate />
+            </div>
+            <Button type="submit">Save changes</Button>
+          </form>
+        </div>
         <DrawerFooter>
-          <Button>Submit</Button>
           <DrawerClose asChild>
             <Button variant="outline">Cancel</Button>
           </DrawerClose>
         </DrawerFooter>
       </DrawerContent>
-    </Drawer>
+    </Drawer >
   )
 }
