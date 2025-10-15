1c4041b74bea441815988a5e496f28374c6ef885
seantanyurong <seantanyurong@gmail.com>
2025-10-15 15:20:54 +0800

    feature: order form

--- app/(no-layout)/order/page.tsx
+++ app/(no-layout)/order/page.tsx
@@ -1,30 +1,144 @@
-"use client";
+'use client';
 
-import { Button } from "@/components/ui/button";
-import { useState } from "react";
+import { useMemo, useState } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import Logo from '@/public/logo.png';
+import { Button } from '@/components/ui/button';
+// import { Checkbox } from '@/components/ui/checkbox';
+// import { Label } from '@/components/ui/label';
+import { Star } from 'lucide-react';
 
 export default function Order() {
-  const [numberOfKnives, setNumberOfKnives] = useState("3");
+  const [numberOfKnives, setNumberOfKnives] = useState(4);
+  const [numberOfRepairs, setNumberOfRepairs] = useState(0);
+  // const [urgentOrder, setUrgentOrder] = useState(false);
 
-  const numberOfKnivesOptions = ["3", "4", "5", "6", "7"];
+  const numberOfKnivesOptions = [4, 5, 7, 10, 14];
+
+  // Prefill WhatsApp message
+  const checkoutHref = useMemo(
+    () =>
+      `/checkout?knives=${encodeURIComponent(
+        numberOfKnives,
+      )}&repairs=${encodeURIComponent(numberOfRepairs)}}`,
+    [numberOfKnives, numberOfRepairs],
+  );
 
   return (
-    <main>
-      <div className="flex flex-col items-center justify-center h-screen">
-        <h1 className="text-3xl mt-6 mb-6 font-bold text-center">
-          Knife Sharpening SG
+    <main className="bg-primary min-h-screen pb-28">
+      <div className="flex justify-center items-center py-6">
+        <Image
+          src={Logo}
+          alt="Knife Sharpening Singapore"
+          width={150}
+          height={90}
+          priority
+        />
+      </div>
+
+      <div className="p-4 max-w-xl mx-auto">
+        <h1 className="text-2xl font-bold text-primary-foreground">
+          Book Knife Sharpening Service
         </h1>
-        <div>
-          {numberOfKnivesOptions.map((option) => (
-            <Button
-              key={option}
-              onClick={() => setNumberOfKnives(option)}
-              className={`${option === numberOfKnives ? "bg-primary" : "bg-muted"} text-white text-sm font-medium py-2 px-4 rounded-lg shadow-md hover:bg-yellow-600 transition`}
-            >
-              {option}
-            </Button>
-          ))}
+
+        <div className="flex items-center gap-1 mt-2">
+          <div className="flex">
+            {[...Array(5)].map((_, index) => (
+              <Star
+                key={index}
+                className="w-4 h-4 text-secondary fill-secondary"
+              />
+            ))}
+          </div>
+          <span className="text-xs text-gray-500 font-medium">(116)</span>
         </div>
+
+        {/* Knives */}
+        <div>
+          <h2 className="text-start mt-8 text-primary-foreground">
+            How Many Knives?
+          </h2>
+          <div className="flex items-center gap-2 mt-2">
+            {numberOfKnivesOptions.map((option) => (
+              <Button
+                key={option}
+                onClick={() => setNumberOfKnives(option)}
+                variant={option === numberOfKnives ? 'secondary' : 'muted'}
+                size="lg"
+                className="w-full"
+              >
+                {option}
+              </Button>
+            ))}
+          </div>
+        </div>
+
+        {/* Repairs */}
+        <div>
+          <h2 className="text-start mt-8 text-primary-foreground">
+            How Many Repairs?
+          </h2>
+          <p className="italic text-primary-foreground text-xs">
+            Repairing small chips are free. Repairing large chips, de-rusting,
+            and straightening blades are all an additional $10.
+          </p>
+          <div className="flex gap-1 mt-2">
+            <Button
+              variant="outline"
+              size="lg"
+              onClick={
+                () => setNumberOfRepairs((r) => Math.max(0, r - 1)) // clamp at 0
+              }
+              aria-label="Decrease repairs"
+            >
+              -
+            </Button>
+            <div
+              className={`${numberOfRepairs > 0 ? 'bg-secondary text-secondary-foreground' : 'bg-muted text-muted-foreground'} h-10 rounded-md px-8 shadow w-full flex justify-center items-center`}
+            >
+              <p>{numberOfRepairs}</p>
+            </div>
+            <Button
+              variant="outline"
+              size="lg"
+              onClick={() => setNumberOfRepairs((r) => r + 1)}
+              aria-label="Increase repairs"
+            >
+              +
+            </Button>
+          </div>
+        </div>
+
+        {/* Urgent */}
+        {/* <div className="mt-8 flex items-center gap-2"> */}
+        {/*   <Checkbox */}
+        {/*     id="urgent" */}
+        {/*     checked={urgentOrder} */}
+        {/*     onCheckedChange={(v) => setUrgentOrder(Boolean(v))} */}
+        {/*     className="bg-white data-[state=checked]:bg-secondary data-[state=checked]:text-secondary-foreground" */}
+        {/*   /> */}
+        {/*   <Label */}
+        {/*     htmlFor="urgent" */}
+        {/*     className="text-primary-foreground cursor-pointer" */}
+        {/*   > */}
+        {/*     Urgent Order? */}
+        {/*   </Label> */}
+        {/* </div> */}
+
+        {/* Bottom actions */}
+        <Button asChild size="lg" variant="secondary" className="w-full mt-8">
+          <Link href={checkoutHref}>Make Payment</Link>
+        </Button>
+        <Button asChild variant="whatsapp" size="lg" className="w-full mt-4">
+          <a
+            href={`https://wa.me/6580684206`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Questions? Ask us on WhatsApp
+          </a>
+        </Button>
       </div>
     </main>
   );
