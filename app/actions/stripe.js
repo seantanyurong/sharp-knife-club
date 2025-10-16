e4308f6a3ec296d09b2504790c7abd9f945b8b84
seantanyurong <seantanyurong@gmail.com>
2025-10-16 14:27:22 +0800

    feature: alternative price ids

--- app/actions/stripe.js
+++ app/actions/stripe.js
@@ -4,8 +4,54 @@ import { headers } from 'next/headers';
 
 import { stripe } from '../../lib/stripe';
 
+function getKnifePriceIdFromKnifeQuantity(knives) {
+  switch (knives) {
+    case 4:
+      return 'price_1SGZZ0GaVAbUPxo7NVHaCdex';
+    case 5:
+      return 'price_1SIkIaGaVAbUPxo7O41lFZVp';
+    case 7:
+      return 'price_1SJ1a0GaVAbUPxo7O41lFZVp';
+    case 10:
+      return 'price_1SJ5a0GaVAbUPxo7O41lFZVp';
+    case 14:
+      return 'price_1SJ9a0GaVAbUPxo7O41lFZVp';
+    default:
+      return 'price_1SGZZ0GaVAbUPxo7NVHaCdex';
+  }
+}
+
+function getRepairPriceIdFromRepairQuantity(repairs) {
+  return 'price_1SIkIaGaVAbUPxo7O41lFZVp';
+}
+
+function generateLineItems(knives, repairs) {
+  const line_items = [];
+
+  const knifePriceId = getKnifePriceIdFromKnifeQuantity(knives);
+  const repairPriceId = getRepairPriceIdFromRepairQuantity(repairs);
+
+  if (knives > 0) {
+    line_items.push({
+      price: knifePriceId,
+      quantity: knives,
+    });
+  }
+
+  if (repairs > 0) {
+    line_items.push({
+      price: repairPriceId,
+      quantity: repairs,
+    });
+  }
+
+  return line_items;
+}
+
 export async function fetchClientSecret(knives, repairs) {
   const origin = (await headers()).get('origin');
+
+  const line_items = generateLineItems(knives, repairs);
 
   const session = await stripe.checkout.sessions.create({
     ui_mode: 'embedded',
@@ -16,16 +62,7 @@ export async function fetchClientSecret(knives, repairs) {
     phone_number_collection: {
       enabled: true,
     },
-    line_items: [
-      {
-        price: 'price_1SGZZ0GaVAbUPxo7NVHaCdex',
-        quantity: knives,
-      },
-      {
-        price: 'price_1SIkIaGaVAbUPxo7O41lFZVp',
-        quantity: repairs,
-      },
-    ],
+    line_items: line_items,
     custom_fields: [
       {
         key: 'additional_instructions',
