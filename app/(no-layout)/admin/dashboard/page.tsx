b48a4c1e45de13d7e11d50002d3410f1038f092d
seantanyurong <seantanyurong@gmail.com>
2025-12-09 16:01:01 +0800

    feat: middleware

---
+++ app/(no-layout)/admin/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+export default function Dashboard() {
+  return (
+    <div className="flex h-screen w-full items-center justify-center">
+      <div className="w-full max-w-md rounded-lg border p-6 shadow-lg">
+        <h1 className="mb-6 text-center text-2xl font-bold">Dashboard</h1>
+      </div>
+    </div>
+  );
+}
