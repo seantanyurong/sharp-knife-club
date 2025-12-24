bf5507ed681736810f203b8b582b0e065e64d38a
seantanyurong <seantanyurong@gmail.com>
2025-12-24 14:53:04 +0800

    feat: add images to order page

--- app/(no-layout)/admin/order/[orderId]/page.tsx
+++ app/(no-layout)/admin/order/[orderId]/page.tsx
@@ -1,5 +1,7 @@
 import ProfileCard from "@/components/admin/ProfileCard";
 import { getPageBody } from "@/app/actions/notion";
+import type { BlockObjectResponse, PartialBlockObjectResponse } from '@notionhq/client'
+import Image from 'next/image';
 
 export default async function OrderPage({
   params,
@@ -10,11 +12,35 @@ export default async function OrderPage({
 
   const pageBody = await getPageBody(orderId);
 
+  const isFullBlock = (block: PartialBlockObjectResponse | BlockObjectResponse) => {
+    return 'type' in block;
+  };
+
+  const convertPageBlockToJsx = (pageBlock: BlockObjectResponse, key: number) => {
+    if (pageBlock.type === 'paragraph') {
+      return <p key={key}>{pageBlock.paragraph.rich_text[0]?.plain_text}</p>
+    }
+
+    if (pageBlock.type === 'image') {
+      if (pageBlock.image.type === 'external') {
+        return <Image key={key} src={pageBlock.image.external.url} width={500} height={500} alt='order image' />
+      }
+
+      if (pageBlock.image.type === 'file') {
+        return <Image key={key} src={pageBlock.image.file.url} width={500} height={500} alt='order image' />
+      }
+    }
+  };
+
   return (
     <div className="p-4 flex justify-center flex-col">
       <ProfileCard name='Order Details' subtitle={orderId} />
-      <div>
-        <pre>{JSON.stringify(pageBody, null, 2)}</pre>
+      <div className='mt-4'>
+        {
+          pageBody?.filter(isFullBlock).map((block, index) => {
+            return convertPageBlockToJsx(block, index);
+          })
+        }
       </div>
     </div>
   )
